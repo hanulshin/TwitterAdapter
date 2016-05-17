3ddd929616dc2a746b66bcea5dc81cc45f851442
Cage Spence <cage.spence@gmail.com>
2016-05-17 21:33:51 +0200

    added cleaned up feed class

--- app/src/main/java/nl/saxion/cage/twitteradapter/Feed.java
+++ app/src/main/java/nl/saxion/cage/twitteradapter/Feed.java
@@ -5,12 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,63 +30,64 @@ public class Feed extends AppCompatActivity {
         List<Tweets> tweets = new ArrayList<>();
 
 
-        CardAdapter adapter= new CardAdapter( this,R.layout.card_item,tweets);
+        CardAdapter adapter = new CardAdapter(this, R.layout.card_item, tweets);
 
         RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
         recList.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recList.setLayoutManager(llm);
-        recList.setAdapter( adapter );
+        recList.setAdapter(adapter);
+
         URL url = null;
         try {
-
-
             url = new URL("http://<url>");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
         HttpURLConnection conn = null;
+
         try {
-            conn = (HttpURLConnection)url.openConnection();
+            conn = (HttpURLConnection) url.openConnection();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         conn.setReadTimeout(10000);
         conn.setConnectTimeout(15000);
         conn.setDoInput(true);
-        int response= 0;
+        int response = 0;
+
         try {
             response = conn.getResponseCode();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(response==200){
-
-        }else {
-
+        if (response == 200) {
+            //everything is okay :D
+        } else {
+            //we fucked up
         }
 
         try {
-            InputStream is=conn.getInputStream();
+            InputStream is = conn.getInputStream();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
 
         try {
             conn.setRequestMethod("GET");
         } catch (ProtocolException e) {
             e.printStackTrace();
         }
+
+        //tells client we want to get input from it
         conn.setDoInput(true);
 
         String file = null;
         try {
             file = readAssetIntoString("tweets.json");
-            JSONObject stateObj	=	new JSONObject(file);
+            JSONObject stateObj = new JSONObject(file);
             JSONArray tweetArray = stateObj.getJSONArray("statuses");
             for (int i = 0; i < tweetArray.length(); i++) {
                 JSONObject tweetObj = tweetArray.getJSONObject(i);
@@ -104,27 +102,24 @@ public class Feed extends AppCompatActivity {
                 String name = userObj.getString("name");
                 String screen_name = userObj.getString("screen_name");
 
-                Users user = new Users(screen_name,name);
+                Users user = new Users(screen_name, name);
 
-                tweets.add( new Tweets( user, text, retweets, createdAt,favourites));
-
+                tweets.add(new Tweets(user, text, retweets, createdAt, favourites));
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-
     }
+
     /**
      * Reads an asset file and returns a string with the full contents.
      *
-     * @param filename  The filename of the file to read.
-     * @return          The contents of the file.
-     * @throws IOException  If file could not be found or not read.
+     * @param filename The filename of the file to read.
+     * @return The contents of the file.
+     * @throws IOException If file could not be found or not read.
      */
     private String readAssetIntoString(String filename) throws IOException {
         BufferedReader br = null;
