899b74fd73ddd5eab88b149ec6f433ddce3f95c4
Cage Spence <cage.spence@gmail.com>
2016-05-17 22:31:07 +0200

    created ConnectionModel class, moved connection stuff there

--- app/src/main/java/nl/saxion/cage/twitteradapter/Feed.java
+++ app/src/main/java/nl/saxion/cage/twitteradapter/Feed.java
@@ -38,55 +38,6 @@ public class Feed extends AppCompatActivity {
         recList.setLayoutManager(llm);
         recList.setAdapter(adapter);
 
-        URL url = null;
-        try {
-            url = new URL("http://<url>");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection conn = null;
-
-        try {
-            conn = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        conn.setReadTimeout(10000);
-        conn.setConnectTimeout(15000);
-        conn.setDoInput(true);
-        int response = 0;
-
-        try {
-            response = conn.getResponseCode();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (response == 200) {
-            //everything is okay :D
-        } else {
-            //we fucked up
-        }
-
-
-        try {
-            conn.setRequestMethod("GET");
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        }
-
-        //tells client we want to get input from it
-        conn.setDoInput(true);
-
-        try {
-            InputStream is = conn.getInputStream();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
 
 
         String file = null;
